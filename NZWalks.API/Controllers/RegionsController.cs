ee51730d7dd6adcc6bc23ad43da3e83194441a82
Arun.Peechanari.Parappurath <Arun.Peechanari.Parappurath@gds.ey.com>
2023-05-17 15:26:04 +0530

    Serilog added

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -8,36 +8,55 @@ using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTO;
 using NZWalks.API.Repositories;
 using System.Data;
+using System.Text.Json;
 
 namespace NZWalks.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class RegionsController : ControllerBase
     {
         private readonly NZWalksDbContext _dbContext;
         private readonly IRegionRepository _regionRepository;
         private readonly IMapper mapper;
-        public RegionsController(NZWalksDbContext dbContext, IRegionRepository regionRepository,IMapper mapper)
+        private readonly ILogger<RegionsController> logger;
+
+        public RegionsController(NZWalksDbContext dbContext, IRegionRepository regionRepository,IMapper mapper,ILogger<RegionsController> logger)
         {
             this._dbContext = dbContext;
             this._regionRepository = regionRepository;
             this.mapper = mapper;
+            this.logger = logger;
         }
 
         [HttpGet]
+        //[Authorize(Roles = "Reader")]
         public async Task<IActionResult> GetAll()
         {
-            var regionDomains = await _regionRepository.GetAllAsync();
-            var regionDto = mapper.Map<List<RegionDto>>(regionDomains);
-            return Ok(regionDto);
+            try
+            {
+                //throw new Exception("This is a custom exception");
+                logger.LogInformation("GetAll method invoked");
+                var regionDomains = await _regionRepository.GetAllAsync();
+                var regionDto = mapper.Map<List<RegionDto>>(regionDomains);
+                logger.LogInformation($"Finished Getall method with data: {JsonSerializer.Serialize(regionDto)}");
+                logger.LogWarning("Test warning");
+                logger.LogError("Test Error");
+                return Ok(regionDto);
+            }
+            catch(Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
         }
 
         // GET SINGLE REGION (Get Region By ID)
         // GET: https://localhost:portnumber/api/regions/{id}
         [HttpGet]
         [Route("{id:Guid}")]
+        [Authorize(Roles = "Reader")]
         public async Task<IActionResult> GetById([FromRoute] Guid id)
         {
             //var region = dbContext.Regions.Find(id);
@@ -50,6 +69,7 @@ namespace NZWalks.API.Controllers
             return Ok(regionDto);
         }
         [HttpPost]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> Create([FromBody] RegionRequestDto regionRequestDto)
         {
             var regionDomainModel = mapper.Map<Region>(regionRequestDto);
@@ -59,6 +79,7 @@ namespace NZWalks.API.Controllers
         }
         [HttpPut]
         [Route("{id:Guid}")]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] RegionRequestDto regionRequestDto)
         {
 
@@ -74,6 +95,7 @@ namespace NZWalks.API.Controllers
         }
         [HttpDelete]
         [Route("{id:Guid}")]
+        [Authorize(Roles = "Writer,Reader")]
         public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
             var regionDomainModel = await _regionRepository.DeleteAsync(id);
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -5,11 +5,20 @@ using Microsoft.IdentityModel.Tokens;
 using NZWalks.API.Data;
 using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
+using Serilog;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/NZWalks_logs.txt",rollingInterval: RollingInterval.Minute)
+    .MinimumLevel.Information()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
